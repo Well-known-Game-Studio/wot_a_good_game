7d87d16b21e2dfa6f07ccb80e6b022ac4ee28518
William Emfinger <waemfinger@gmail.com>
2022-02-25 20:16:55 -0600

    removed test voxel projects from repo (testing c++ and voxelPlugin in different engine versions)

--- voxel_cpp_test/Source/voxel_cpp_test/voxel_cpp_test.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "voxel_cpp_test.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, voxel_cpp_test, "voxel_cpp_test" );
-
-DEFINE_LOG_CATEGORY(Logvoxel_cpp_test)
- 
--- voxels_of_time/Source/voxels_of_time/voxels_of_time.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "voxels_of_time.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, voxels_of_time, "voxels_of_time" );
-
-DEFINE_LOG_CATEGORY(Logvoxels_of_time)
- 
