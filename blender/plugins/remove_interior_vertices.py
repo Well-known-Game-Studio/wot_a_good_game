56447372c452065f757459b1a181e41443d0950f
William Emfinger <waemfinger@gmail.com>
2022-03-05 14:19:22 -0600

    working on script to select interior vertices.

---
+++ blender/plugins/remove_interior_vertices.py
@@ -0,0 +1,14 @@
+import bpy, bmesh
+# Get selected object's mesh
+mesh = bpy.context.object.data  
+# Create new edit mode bmesh to easily acces mesh data
+bm = bmesh.from_edit_mesh(mesh) 
+# go through all the vertices of the model
+for v in bm.verts:
+    # select only vertices which have 6 (all) edges
+    correct_edges = len(v.link_edges) == 6
+    # and which are connected to all possible 12 faces
+    correct_faces = len(v.link_faces) == 12
+    v.select_set(correct_edges and correct_faces)
+# Transfer the data back to the object's mesh
+bmesh.update_edit_mesh(mesh)
