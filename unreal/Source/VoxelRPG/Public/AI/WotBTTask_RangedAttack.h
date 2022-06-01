e5881fcf13a8ecff13111f5310806cd43d204f94
William Emfinger <waemfinger@gmail.com>
2022-05-31 21:40:59 -0500

    tut: working through ranged attack BehaviorTree task for AI minion.

---
+++ unreal/Source/VoxelRPG/Public/AI/WotBTTask_RangedAttack.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "WotBTTask_RangedAttack.generated.h"
+
+UCLASS()
+class VOXELRPG_API UWotBTTask_RangedAttack : public UBTTaskNode
+{
+  GENERATED_BODY()
+
+  virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+protected:
+
+  UPROPERTY(EditAnywhere, Category = "AI")
+  TSubclassOf<AActor> ProjectileClass;
+};
