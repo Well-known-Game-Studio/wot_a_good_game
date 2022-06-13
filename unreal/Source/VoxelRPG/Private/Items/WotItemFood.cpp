71f8c5f74db19c21647ce6980f5d7352a519e707
William Emfinger <waemfinger@gmail.com>
2022-06-13 08:09:18 -0500

    bugfix: fooditem now properly can be eaten and heals character

--- unreal/Source/VoxelRPG/Private/Items/WotItemFood.cpp
+++ unreal/Source/VoxelRPG/Private/Items/WotItemFood.cpp
@@ -12,15 +12,21 @@ void UWotItemFood::Use(ACharacter* Character)
 {
   // Make sure this is still a valid character
   if (!Character) {
+    UE_LOG(LogTemp, Warning, TEXT("Character is null!"));
     return;
   }
   // Get the Attribute Component for this character
   UWotAttributeComponent* AttributeComp = Cast<UWotAttributeComponent>(Character->GetComponentByClass(UWotAttributeComponent::StaticClass()));
-  if (AttributeComp) {
+  if (!AttributeComp) {
+    UE_LOG(LogTemp, Warning, TEXT("Attribute Component is null!"));
     return;
   }
   // And Heal Them
-  AttributeComp->ApplyHealthChange(HealingAmount);
+  bool WasUsed = AttributeComp->ApplyHealthChange(HealingAmount);
+  if (!WasUsed) {
+    UE_LOG(LogTemp, Warning, TEXT("Full health!"));
+    return;
+  }
   // remove it from the inventory
   if (OwningInventory) {
     OwningInventory->RemoveItem(this);
