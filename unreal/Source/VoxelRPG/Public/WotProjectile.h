1cf37410bb182d59003aae5ad0a8ea1a8abd33a0
William Emfinger <waemfinger@gmail.com>
2023-09-02 17:33:30 -0500

    feat(crate): updated geometry tools and added breakables * Updated projectile base class to allow blueprint to override ShouldOverlap (useful for magic projectiles) * Updated attribute component to allow health to be overridden in level instances of an object * Updated base voxel subsurface material to enable new types of materials such as metals * Added voxel metal material * Updated basic wall tool to better support 3d edges * Made crate with new wall tool * Used fracture tool to fracture crate with a grid into geometry collection * Make bp classes for breakables;

--- unreal/Source/VoxelRPG/Public/WotProjectile.h
+++ unreal/Source/VoxelRPG/Public/WotProjectile.h
@@ -26,7 +26,10 @@ public:
 
 protected:
 
-  UFUNCTION(BlueprintCallable)
+  UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Impact")
+  bool ShouldHitActor(AActor* OtherActor, UPrimitiveComponent* OtherComp);
+
+  UFUNCTION(BlueprintCallable, Category = "Impact")
   virtual void OnActorOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
   UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "Interaction")
