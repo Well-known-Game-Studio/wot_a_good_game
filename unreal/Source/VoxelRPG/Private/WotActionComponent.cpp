f9734ded0db088fd620754770ad8f5bcdd7f1313
William Emfinger <waemfinger@gmail.com>
2025-05-31 23:05:37 -0500

    fix: Bug when opening menu while sprint jumping (#110)

--- unreal/Source/VoxelRPG/Private/WotActionComponent.cpp
+++ unreal/Source/VoxelRPG/Private/WotActionComponent.cpp
@@ -34,7 +34,7 @@ void UWotActionComponent::AddAction(TSubclassOf<UWotAction> ActionClass)
   }
 }
 
-bool UWotActionComponent::StartActionByName(AActor* Instigator, FName ActionName)
+bool UWotActionComponent::StartActionByName(FName ActionName, AActor* Instigator)
 {
   for (UWotAction* Action : Actions) {
     if (Action && Action->ActionName == ActionName) {
@@ -51,7 +51,7 @@ bool UWotActionComponent::StartActionByName(AActor* Instigator, FName ActionName
   return false;
 }
 
-bool UWotActionComponent::StopActionByName(AActor* Instigator, FName ActionName)
+bool UWotActionComponent::StopActionByName(FName ActionName, AActor* Instigator)
 {
   for (UWotAction* Action : Actions) {
     if (Action && Action->ActionName == ActionName) {
@@ -64,10 +64,42 @@ bool UWotActionComponent::StopActionByName(AActor* Instigator, FName ActionName)
   return false;
 }
 
+bool UWotActionComponent::StopAllActions(AActor* Instigator)
+{
+  bool bStoppedAnyAction = false;
+  for (UWotAction* Action : Actions) {
+    if (Action && Action->IsRunning()) {
+      Action->Stop(Instigator);
+      bStoppedAnyAction = true;
+    }
+  }
+  return bStoppedAnyAction;
+}
+
 UWotActionComponent* UWotActionComponent::GetActions(AActor* FromActor)
 {
 	if (FromActor) {
 		return Cast<UWotActionComponent>(FromActor->GetComponentByClass(UWotActionComponent::StaticClass()));
 	}
 	return nullptr;
+}
+
+bool UWotActionComponent::IsActionRunning(FName ActionName) const
+{
+  for (const UWotAction* Action : Actions) {
+    if (Action && Action->ActionName == ActionName && Action->IsRunning()) {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool UWotActionComponent::IsAnyActionRunning() const
+{
+  for (const UWotAction* Action : Actions) {
+    if (Action && Action->IsRunning()) {
+      return true;
+    }
+  }
+  return false;
 }
